866671fd3c73070dfd38a05c1e213c14a5515307
Ahmad Rehan <a.rehan@mail.utoroto.ca>
2021-10-21 13:11:37 -0700

    load plugins listed in txt. get DOM tree using jsdom.

---
+++ main.js
@@ -0,0 +1,13 @@
+import * as module from './plugins/index.js';
+import * as fs from 'fs';
+import { JSDOM } from "jsdom";
+
+const html = fs.readFileSync("./page.html");
+const dom = new JSDOM(html);
+console.debug(dom.window.document.body.innerHTML);
+
+let plugins = module.plugins;
+let report = new Array();
+
+plugins.forEach(plugin => report.push(plugin()));
+report.forEach(element => console.debug(element));
---
+++ plugins/3.js
@@ -0,0 +1,3 @@
+export function fn(){
+    return "plugin 3";
+}
