7cfc7941da657e09461dad60a3ceb44ee15764b6
AhmadRehan98 <ahmedrehan98@gmail.com>
2021-10-29 16:02:01 -0700

    added run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-echo $1 $2
-node --experimental-specifier-resolution=node main.js $1 $2
+
+usage(){
+	echo "Usage: sh run.sh <html file>\n" >&2
+	exit 2
+}
+
+DOE(){
+	echo "$1" " does not exist" >&2
+	exit 1
+}
+
+if [ "$#" -ne 1 ]
+then
+	usage
+fi
+
+file="$1"
+
+if [ ! -f "$file" ]
+then
+	DOE "$file"
+fi
+
+node --experimental-specifier-resolution=node main.js "$file"
