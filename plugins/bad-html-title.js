203d3a74997400c0b06bc588f70f36d12737a9d7
AhmadRehan98 <ahmedrehan98@gmail.com>
2021-11-19 18:20:28 -0800

    renamed files. implemented plugin #7 with very few bad titles

---
+++ plugins/bad-html-title.js
@@ -0,0 +1,27 @@
+
+export function fn(dom){
+    let output = "Error: HTML missing or bad title";
+    let errors = 0;
+    let badTitles = ["untitled", "no title", "new page", "untitled page", "new page"]
+    if (dom.window.document.head == null || 
+        dom.window.document.title == null){
+            errors++;
+        }
+    else{
+        let title = dom.window.document.title.toLowerCase();
+        // console.debug(title);
+        if (title.length == 0 || badTitles.includes(title)){
+            errors++;
+        }
+        else{
+            // console.debug(title);
+            errors = 0;
+        }
+    }
+
+    if (errors === 0) return ["", errors];
+    return [output, errors];
+
+}
+
+export default fn;
