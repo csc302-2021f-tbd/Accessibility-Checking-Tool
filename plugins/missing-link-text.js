f5463e0310a99c54174901b6aa9faf0da6b40edf
Ben Grass <bvgrass@outlook.com>
2021-12-06 13:32:54 -0500

    missing link text plugin

--- plugins/missing-link-text.js
+++ plugins/missing-link-text.js
@@ -2,8 +2,29 @@ export function fn(dom) {
     let output = "Error: missing/empty link text ";
     let errors = 0;
 
-    // TODO: function body for plugin 4
+    if (dom.window.document.body == null){
+        return ["", 0];
+    }
+    else {
+        let body = dom.window.document.body;
+        output = helper(dom, body.children);
+        errors = output.length;
+        // console.debug(errors);
+    }
 
     if (errors === 0) return ["", errors];
+    let output = "Error: missing/empty link text ";
     return [output, errors];
+}
+
+export function helper(dom, children, output = []) {
+    for (let i=0; i < children.length; i++) {
+        let item = children.item(i);
+        // TODO: think about whether this check needs to be more detailed
+        if (item.tagName === "A" && !(item.innerHTML === ""))
+            output.push("Error: " + item.outerHTML);
+
+        if (item.children.length > 0) 
+            output = helper(dom, item.children, output);
+    }
 }
