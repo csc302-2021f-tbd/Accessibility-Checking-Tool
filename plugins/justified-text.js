18f847e6913df82dc2c0c75e156a6e78e8e34c66
Ben Grass <bvgrass@outlook.com>
2021-12-06 10:36:42 -0500

    completed justified text plugin

--- plugins/justified-text.js
+++ plugins/justified-text.js
@@ -2,8 +2,31 @@ export function fn(dom) {
     let output = "Error: use of justified text alignment ";
     let errors = 0;
 
-    // TODO: function body for plugin 10
+    if (dom.window.document.body == null){
+        return ["", 0];
+    }
+    else {
+        let body = dom.window.document.body;
+        output = helper(dom, body.children);
+        errors = output.length;
+        // console.debug(errors);
+    }
 
     if (errors === 0) return ["", errors];
     return [output, errors];
 }
+
+export function helper(dom, children, output = []) {
+    for (let i=0; i < children.length; i++) {
+        let item = children.item(i);
+        if (item.style.textAlign === "justify" ||  
+            item.align === "justify") 
+            
+            output.push("Error: " + item.outerHTML);
+
+        if (item.children.length > 0) 
+            output = helper(dom, item.children, output);
+    }
+}
+
+export default fn;
